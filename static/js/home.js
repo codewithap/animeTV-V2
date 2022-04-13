2429c99f4865b96808a464686ea4f925fed4a04d
codewithap <99323868+codewithap@users.noreply.github.com>
2022-04-13 08:32:02 +0530

    Add files via upload

--- static/js/home.js
+++ static/js/home.js
@@ -56,3 +56,21 @@ nightModeBtn.addEventListener("click",()=>{
         localStorage.setItem("nightMode",false)
     }
 });
+
+function readMoreLess() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
