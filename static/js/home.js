49f0f116c3b243b1bc921a65094c035c9b083c6c
arijitpaine647@gmail.com <arijitpaine647@gmail.com>
2022-09-06 07:23:49 +0530

    🗺️

--- static/js/home.js
+++ static/js/home.js
@@ -63,13 +63,13 @@ function readMoreLess() {
     var btnText = document.getElementById("myBtn");
   
     if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
     } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
     }
   }
 
